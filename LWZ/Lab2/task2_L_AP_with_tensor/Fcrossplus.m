667972b5fa98dd439d44bf234fc991708d1c01cf
octogen4 <lwz_lwzlwz@163.com>
2022-02-11 00:17:07 +0800

    lab topic2 task1,2

--- LWZ/Lab2/task2_L_AP_with_tensor/Fcrossplus.m
+++ LWZ/Lab2/task2_L_AP_with_tensor/Fcrossplus.m
@@ -6,11 +6,20 @@ ny=[0 1 0];
 nz=[0 0 1];
 
 % Source direction vector in detector frame
-nv=nv(theta,phi);
+[nvx,nvy,nvz]=dirunitvect(theta,phi);
+nv=[nvx,nvy,nvz];
 
 % Wave frame base
 xv=cross(nz,nv);
 yv=cross(xv,nv);
+if(norm(xv)>0)
+    xv=xv/norm(xv);
+end
+
+if(norm(yv)>0)
+    yv=yv/norm(yv);
+end
+    
 
 % Detector tensor
 D=(nx'*nx-ny'*ny)/2;
@@ -20,7 +29,7 @@ eplus=xv'*xv-yv'*yv;
 ecross=xv'*yv+yv'*xv;
 
 % Antenna patterns
-Fplus=sum(D.*eplus);
-Fcross=sum(D.*ecross);
+Fplus=sum(D(:).*eplus(:));
+Fcross=sum(D(:).*ecross(:));
 
 end
--- LWZ/Lab2/task2_L_AP_with_tensor/testapwithtensor.m
+++ LWZ/Lab2/task2_L_AP_with_tensor/testapwithtensor.m
@@ -8,19 +8,26 @@ X=sin(thetaV).*cos(phiV);
 Y=sin(thetaV).*sin(phiV);
 Z=cos(thetaV);
 
-c=fplus(thetaV,phiV);
+fplusmat = zeros(length(phis),length(thetas));
+fcrossmat = zeros(length(phis),length(thetas));
+
+for i = 1:length(phis)
+    for j = 1:length(thetas)
+        [fcrossmat(i,j),fplusmat(i,j)] = Fcrossplus(thetas(j),phis(i));
+    end
+end
 
 fig=figure;fig.Position=[60,50,1100,400];
 subplot(1,2,1);
-c=fplus(thetaV,phiV);
+c=fplusmat;
 surf(X,Y,Z,abs(c));
 shading interp;
 colorbar;
-title('f plus');
+title('f plus (with tensor)');
 subplot(1,2,2);
-c=fcross(thetaV,phiV);
+c=fcrossmat;
 surf(X,Y,Z,abs(c));
 shading interp;
 colormap(jet);
 colorbar;
-title('f cross');
+title('f cross (with tensor)');
